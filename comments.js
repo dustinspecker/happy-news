69c169e9dc28f8c21a29b9dd356b2cf454ffe368
Dustin Specker <DustinSpecker@DustinSpecker.com>
2016-05-21 17:26:29 -0500

    feat(comments): add more nice comments

--- comments.js
+++ comments.js
@@ -1,9 +1,16 @@
 window.happyComments =
   [ 'Awesome job! Keep it up!'
+  , 'Day made!'
   , 'Genius idea!'
+  , 'Groovy!'
   , 'I love seeing people try stuff like this!'
   , 'I love this!'
   , 'I\'m showing this to all of my coworkers!'
+  , 'Just unbelievable!'
+  , 'Love it!'
+  , 'Marvelous!'
+  , 'Nice!',
+  , 'Nice job!'
   , 'Sharing on Facebook!',
   , 'Sharing on Instagram!',
   , 'Sharing on Snapchat!',
@@ -11,8 +18,12 @@ window.happyComments =
   , 'Sharing on Tumblr!',
   , 'Sharing on Twitter!',
   , 'Take my money!'
+  , 'The force is strong with this one!'
   , 'This is incredible!'
+  , 'This is going to help so many people!'
   , 'Truly inspiring!'
   , 'You rock!'
+  , 'We are really going place now!'
   , 'Whoa!'
+  , 'Wow! I never knew I needed this until now.'
   ]
